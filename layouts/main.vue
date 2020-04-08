3aca736d9bae2d002a85932836074160883af628
1577351xyh <564146958@qq.com>
2020-04-08 23:02:00 +0800

    router-view 底部前进后退实现

--- layouts/main.vue
+++ layouts/main.vue
@@ -4,7 +4,14 @@
     <leftNav></leftNav>
     <div class="content" ref="content">
       <router-view></router-view>
-      <div>底部前进后退</div>
+      <div class="footer">
+        <span @click="go(routerFooter[1].path)">{{
+          routerFooter[1].name || ''
+        }}</span>
+        <span @click="go(routerFooter[0].path)">{{
+          routerFooter[0].name || ''
+        }}</span>
+      </div>
     </div>
     <right-nav class="right-nav" :data="navArr"></right-nav>
     <!-- 右侧导航栏 -->
@@ -17,25 +24,62 @@ export default {
   data() {
     return {
       navArr: [],
+      routerIndex: '',
+      routerFooter: [],
     }
+  },
+  created() {
+    this.createdFooter()
   },
   watch: {
     $route(to, from) {
       this.$nextTick(() => {
+        this.routerFooter = []
         this.navArr = []
         this.createdDom()
+        this.createdFooter()
       })
     },
   },
   methods: {
-    createdDom() {
-      let el = this.$refs.content.querySelector('.markdown')
-      if (el.getElementsByTagName('h3')) {
-        let navArr = el.getElementsByTagName('h3')
-        console.log(navArr)
-        navArr.forEach((vm) => {
-          this.navArr.push(vm.innerText)
+    createdFooter() {
+      let cuurrPuoter = this.$router.currentRoute.path
+      this.$router.options.routes[1].children.forEach((vm, index, arr) => {
+        if (cuurrPuoter.indexOf(vm.path) > -1) {
+          this.routerIndex = index
+          if (index + 1 !== arr.length) {
+            this.routerFooter.push(this.obj(index + 1), this.obj(index - 1))
+          } else {
+            this.routerFooter.push(this.obj(-1), this.obj(index - 1))
+          }
+        }
+      })
+    },
+    go(path) {
+      this.$router.push(path)
+    },
+    obj(index) {
+      let obj = {}
+      if (index === -1) {
+        return (obj = {
+          path: '',
+          name: '',
         })
+      }
+      return (obj = {
+        path: this.$router.options.routes[1].children[index].path,
+        name: this.$router.options.routes[1].children[index].name,
+      })
+    },
+    createdDom() {
+      if (this.$refs.content) {
+        let el = this.$refs.content.querySelector('.markdown')
+        if (el && el.getElementsByTagName('h3')) {
+          let navArr = el.getElementsByTagName('h3')
+          navArr.forEach((vm) => {
+            this.navArr.push(vm.innerText)
+          })
+        }
       }
     },
   },
@@ -57,6 +101,12 @@ export default {
   .content {
     flex-grow: 1;
     text-align: left;
+    margin: 10px;
+    .footer {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 30px;
+    }
   }
   .right-nav {
     width: 200px;
