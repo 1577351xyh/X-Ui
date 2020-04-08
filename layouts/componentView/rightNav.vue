e9d8b20c1f9b2b6b6dcc76af7bac035d2dd5e09f
1577351xyh <564146958@qq.com>
2020-04-08 21:44:39 +0800

    main右侧导航

---
+++ layouts/componentView/rightNav.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="containav-right">
+    <ul>
+      <li class="title">导航</li>
+      <li v-for="(item, index) in data" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      requier: true,
+    },
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+<style lang="less" scope>
+.containav-right {
+  > ul {
+    margin: 16px 0;
+    padding-left: 0;
+    font-size: 12px;
+    padding-left: 26px;
+    list-style: none;
+    border-left: 1px solid #f0f0f0;
+    > li {
+      text-align: left;
+      margin-left: 0;
+      padding-left: 0;
+      line-height: 1.5;
+      list-style: none;
+      cursor: pointer;
+      font-size: 14px;
+    }
+    .title {
+      color: #1989fa;
+      font-size: 16px;
+      margin-bottom: 5px;
+    }
+  }
+}
+</style>
--- layouts/main.vue
+++ layouts/main.vue
@@ -1,20 +1,50 @@
 <template>
   <div class="main-cnt">
     <!-- 左侧导航栏 -->
-      <leftNav></leftNav>
+    <leftNav></leftNav>
+    <div class="content" ref="content">
       <router-view></router-view>
+      <div>底部前进后退</div>
+    </div>
+    <right-nav class="right-nav" :data="navArr"></right-nav>
     <!-- 右侧导航栏 -->
-    <!-- 底部前进后退导航 -->
   </div>
 </template>
 <script>
 import leftNav from './componentView/main-menu.vue'
+import rightNav from './componentView/rightNav'
 export default {
   data() {
-    return {}
+    return {
+      navArr: [],
+    }
+  },
+  watch: {
+    $route(to, from) {
+      this.$nextTick(() => {
+        this.navArr = []
+        this.createdDom()
+      })
+    },
+  },
+  methods: {
+    createdDom() {
+      let el = this.$refs.content.querySelector('.markdown')
+      if (el.getElementsByTagName('h3')) {
+        let navArr = el.getElementsByTagName('h3')
+        console.log(navArr)
+        navArr.forEach((vm) => {
+          this.navArr.push(vm.innerText)
+        })
+      }
+    },
+  },
+  mounted() {
+    this.createdDom()
   },
   components: {
     leftNav,
+    rightNav,
   },
 }
 </script>
@@ -23,6 +53,13 @@ export default {
   max-width: 1240px;
   margin: 0 auto;
   display: flex;
-
+  justify-content: space-between;
+  .content {
+    flex-grow: 1;
+    text-align: left;
+  }
+  .right-nav {
+    width: 200px;
+  }
 }
 </style>
